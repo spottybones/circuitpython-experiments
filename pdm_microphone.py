571ea5ca4cdb69726add9dc4db3f2c45fa75965f
Scott Burns <sburns@centercitypcs.org>
2022-07-09 16:42:05 -0400

    Add `pdm_microphone.py` Script
    From: https://learn.adafruit.com/circuitpython-on-the-arduino-nano-rp2040-connect/pdm-microphone
    this script listens on the board's build in microphone and lights an LED
    when sound is heard.

---
+++ pdm_microphone.py
@@ -0,0 +1,48 @@
+# SPDX-FileCopyrightText: 2021 Liz Clark for Adafruit Industries
+#
+# SPDX-License-Identifier: MIT
+
+import time
+import array
+import math
+import board
+import audiobusio
+import pwmio
+import simpleio
+
+# LED setup
+led = pwmio.PWMOut(board.A1, frequency=5000, duty_cycle=0)
+
+# Remove DC bias before computing RMS
+def mean(values):
+    return sum(values) / len(values)
+
+
+def normalized_rms(values):
+    minbuf = int(mean(values))
+    samples_sum = sum(float(sample - minbuf) * (sample - minbuf) for sample in values)
+
+    return math.sqrt(samples_sum / len(values))
+
+
+mic = audiobusio.PDMIn(
+    board.MICROPHONE_CLOCK, board.MICROPHONE_DATA, sample_rate=16000, bit_depth=16
+)
+
+samples = array.array("H", [0] * 160)
+
+while True:
+    mic.record(samples, len(samples))
+    magnitude = normalized_rms(samples)
+    print((magnitude,))
+
+    # mapping mic's level to LED PWM range
+    mapped_value = simpleio.map_range(magnitude, 75, 300, 0, 65535)
+
+    # sending mapped value to LED for PWM
+    led.duty_cycle = int(mapped_value)
+
+    # optional logging for mapped value
+    # print((mapped_value,))
+
+    time.sleep(0.01)
