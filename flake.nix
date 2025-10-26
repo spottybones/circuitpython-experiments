f6817ab871d02a5732eec37c2f95207d1ebcf390
Scott Burns <scott@lentigo.net>
2025-10-26 10:04:40 -0400

    feat(just-lsp): add Just LSP
    Set up Nix devShell to install just-lsp.

---
+++ flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "devShell for Python projects";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+  };
+
+  outputs =
+    inputs@{
+      self,
+      nixpkgs,
+      flake-parts,
+      ...
+    }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = builtins.attrNames nixpkgs.legacyPackages;
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        {
+          devShells.default = pkgs.mkShell {
+            name = "python devShell";
+            buildInputs = with pkgs; [
+              just-lsp
+            ];
+          };
+        };
+    };
+}
